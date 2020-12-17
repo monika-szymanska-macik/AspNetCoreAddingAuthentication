37e155b62574c329ab47733d3dc3733a25a0c89c
Monika <m.szymanska.macik@gmail.com>
2020-12-17 20:21:22 +0100

    Called UseAuthentication in Configure

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -31,7 +31,7 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
+            app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
         }
     }
