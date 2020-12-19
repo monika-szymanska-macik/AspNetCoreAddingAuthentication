14891993468d38d53a774d66b0499d717ccf9f22
Monika <m.szymanska.macik@gmail.com>
2020-12-19 19:31:39 +0100

    created relationship between item and applicationuser model

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -29,16 +29,25 @@ namespace WishList.Controllers
         [HttpGet, AllowAnonymous]
         public IActionResult Register()
         {
-            return View("Register");
+            return View();
         }
         [HttpPost, AllowAnonymous]
-        public IActionResult Register(RegisterViewModel a)
+        public IActionResult Register(RegisterViewModel model)
         {
             if(!ModelState.IsValid)
             {
-                return View("Register");
+                return View(model);
             }
-            return RedirectToAction("HomeController.Index");
+            var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
+            if(!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", error.Description);
+                }
+                return View(model);
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
